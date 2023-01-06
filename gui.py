fdaa3b12c8c67a21ece5335aa7c26daa1cdc129c
dev sys <ataime15@gmail.com>
2023-01-06 23:14:35 +0100

    Implemented the Add button

--- gui.py
+++ gui.py
@@ -2,18 +2,24 @@ from functions import get_todos, save_todo
 import PySimpleGUI as sg #sg == simple gui
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+close_btn = sg.Button("Close") 
 
-window = sg.Window("My To_Do App", layout=[[label], [input_box, add_button]])
-# window.read() #This displays the window on the screen #The biggest and most useful method
+window = sg.Window("My To_Do App", layout=[[label], [input_box, add_button], [close_btn]],
+                        font=('Helvetica', 20))
 
 # Event Loop to process "events" and get the "values" of the inputs
-# while True:
-values = window.read()
+while True:
+    event, values = window.read() #This displays the window on the screen #The biggest and most useful method
+    print(event, values)
+    if event == 'Add':
+        todos_li = get_todos()
+        new_todo = values['todo'] + '\n'
+        todos_li.append(new_todo)
+        save_todo(todos_li)
+    elif event == sg.WIN_CLOSED or event == 'Close': # if user closes window or clicks cancel
+        break #This break statement enable window.close() to be accesible
 
-# if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-    # break
-print('You entered ', values)
 
-window.close()
+window.close() # Window is closed only after the while loop ends
