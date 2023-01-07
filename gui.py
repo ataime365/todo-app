43fbfced537fddff38dea9dd5822488505058f85
dev sys <ataime15@gmail.com>
2023-01-07 21:34:57 +0100

    Implemented the Complete button

--- gui.py
+++ gui.py
@@ -8,9 +8,10 @@ edit_button = sg.Button("Edit")
 close_btn = sg.Button("Close")
 list_box = sg.Listbox(values=get_todos(), key='todos_list', enable_events=True, size=[45,10]) 
 edit_btn = sg.Button("Edit")
+complete_btn = sg.Button("Complete")
 
 window = sg.Window("My To_Do App", layout=[[label], [input_box, add_button], 
-                                            [list_box , edit_btn],
+                                            [list_box , edit_btn, complete_btn],
                                             [close_btn]],
                         font=('Helvetica', 20))
 
@@ -25,6 +26,7 @@ while True:
         save_todo(todos_li)
         list_box1 = window['todos_list']
         list_box1.update(values=todos_li)
+        window['todo'].update(value='') #udating the input_box to be empty
     elif event == "Edit":
         todo_to_edit = values['todos_list'][0] #Edit {'todo': 'dnfjdnfkd', 'todos_list': ['I want to eat\n']}
         new_todo = values['todo']
@@ -37,6 +39,16 @@ while True:
         save_todo(all_todo) #updating the todos.txt file
         list_box1 = window['todos_list']
         list_box1.update(values=all_todo) #updating the widget #values is used for list
+    elif event == "Complete":
+        todo_to_remove = values['todos_list'][0] #Listbox
+        todos_li = get_todos()
+        # index = todos_li.index(todo_to_remove)
+        print(todo_to_remove)
+        todos_li.remove(todo_to_remove) #pop also works, with python indexing
+        save_todo(todos_li)
+        list_box1 = window['todos_list']
+        list_box1.update(values=todos_li) #updating the widget #values is used for list
+        window['todo'].update(value='') #udating the input_box to be empty
     elif event == "todos_list": #This event happens when we click an element in the listbox
         current_list_box_value = values['todos_list'][0] #todos_list {'todo': 'Read the books well', 'todos_list': ['Read the books well\n']}
         input_box1 = window['todo'] #using the key of an element to access it from inside the window #value is used for a string
